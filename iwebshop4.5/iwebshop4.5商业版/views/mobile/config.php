726702210a53016159b54e22b635f1a35b6bf2a4
sageknight@126.com <root@localhost.localdomain>
2016-07-26 15:38:57 +0800

    Enterprise Edition4.5...

---
+++ iwebshop4.5/iwebshop4.5商业版/views/mobile/config.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+	'name'    => 'iWebShop手机版本',
+	'author'  => 'aircheng',
+	'time'    => '2016/4/3 11:41:34',
+	'version' => '4.4',
+	'thumb'   => 'preview.jpg',
+	'info'    => 'Jooyea旗下，IwebShop产品首款默认手机主题方案，此主题适用于IwebShop 4.4 系列产品',
+	'layout'  => array('simple' => 'site','ucenter' => 'site'),
+);
+?>
